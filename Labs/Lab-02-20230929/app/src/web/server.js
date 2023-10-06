3cdc1f6a62d781428b004dd8ef5ae4816819e678
David <david.cohen@studio.unibo.it>
2023-10-06 15:27:48 +0200

    fix nodejs connection

--- Labs/Lab-02-20230929/app/src/web/server.js
+++ Labs/Lab-02-20230929/app/src/web/server.js
@@ -5,19 +5,51 @@ const app = express();
 
 const wss = new WebSocket.Server({ noServer: true });
 
+let javaWs;
+
+function connectToJavaWebSocket() {
+    javaWs = new WebSocket('ws://localhost:8080');
+
+    javaWs.on('open', () => {
+        console.log('Connected to Java WebSocket Server!');
+    });
+
+    javaWs.on('message', (data) => {
+        console.log('Received from Java:', data);
+        // Broadcast data to all connected clients
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data.toString());
+            }
+        });
+    });
+
+    javaWs.on('error', (error) => {
+        console.error('Error connecting to Java WebSocket Server:', error.message);
+    });
+
+    javaWs.on('close', () => {
+        console.error('Connection to Java WebSocket Server closed, retrying in 2 seconds...');
+        setTimeout(connectToJavaWebSocket, 2000);
+    });
+}
+
+// Initially try to connect
+connectToJavaWebSocket();
+
 wss.on('connection', (ws) => {
     console.log('Client connected');
 
     ws.on('message', (message) => {
         console.log('Received:', message);
         if (message === 'requestUpdate') {
-            // Forward this to the Java WebSocket server
-            javaWs.send('requestUpdate');
+            // Check if the connection to the Java WebSocket server is open before sending a message
+            if (javaWs && javaWs.readyState === WebSocket.OPEN) {
+                javaWs.send('requestUpdate');
+            } else {
+                console.error('Cannot forward message, not connected to Java WebSocket Server');
+            }
         }
-    });
-
-    javaWs.on('message', (data) => {
-        ws.send(data.toString());  // Forward data to the client
     });
 });
 

e1cd980bb72c53220128b739d3cd95554888450b
Alessandro Ricci <a.ricci@unibo.it>
2023-12-01 08:58:35 +0100

    refined simple test

--- Labs/Lab-11-20231201/ObsTests/app/src/main/java/sap/observability/TestZipkin.java
+++ Labs/Lab-11-20231201/ObsTests/app/src/main/java/sap/observability/TestZipkin.java
@@ -22,6 +22,17 @@ public class TestZipkin {
 
 		// Tracing exposes objects you might need, most importantly the tracer
 		var tracer = tracing.tracer();
+		
+		// Start a new trace or a span within an existing trace representing an operation
+		var span = tracer.startScopedSpan("encode");
+		
+		try {
+			Thread.sleep(1000);
+		} catch (Exception ex) {
+		  span.error(ex); // Unless you handle exceptions, you might not know the operation failed!
+		} finally {
+		  span.finish(); // always finish the span
+		}
 
 		// Failing to close resources can result in dropped spans! When tracing is no
 		// longer needed, close the components you made in reverse order. This might be
